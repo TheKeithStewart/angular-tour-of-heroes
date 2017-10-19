299f093a8cb12602def4194258580226498ba19e
Keith Stewart <kstewart@collab.net>
2017-10-19 05:46:14 -0500

    add tests

--- src/app/characters.component.spec.ts
+++ src/app/characters.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { CharacterService } from './character.service';
@@ -9,11 +9,14 @@ import { Character } from './hero';
 class MockCharacterService {
   getCharacters = jasmine.createSpy('getCharacters')
     .and.returnValue(new Promise((resolve, reject) => { resolve([]) }));
+  delete = jasmine.createSpy('delete')
+    .and.returnValue(new Promise((resolve, reject) => { }))
 }
 
 describe('CharactersComponent', () => {
   let component: CharactersComponent;
   let fixture: ComponentFixture<CharactersComponent>;
+  let characterService: CharacterService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,6 +33,10 @@ describe('CharactersComponent', () => {
   }));
 
   beforeEach(() => {
+    characterService = TestBed.get(CharacterService);
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(CharactersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -37,6 +44,85 @@ describe('CharactersComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#getCharacters', () => {
+    it('should get characters', () => {
+      component.getCharacters();
+      expect(characterService.getCharacters).toHaveBeenCalled();
+    });
+
+    it('should display message if there is an error', fakeAsync(() => {
+      const error = 'No characters for you!!!';
+      characterService.getCharacters = jasmine.createSpy('getCharacters')
+        .and.returnValue(new Promise((resolve, reject) => { reject(error) }));
+
+      component.getCharacters();
+      tick();
+      expect(component.error).toEqual(error);
+    }));
+  });
+
+  describe('#close', () => {
+    it('should get characters if a character was saved', () => {
+      const getCharacters = spyOn(component, 'getCharacters');
+      const savedCharacter = new Character();
+      component.addingCharacter = true;
+
+      component.close(savedCharacter);
+      expect(component.addingCharacter).toEqual(false);
+      expect(getCharacters).toHaveBeenCalled();
+    });
+
+    it('should no get characters if no characters were saved', () => {
+      const getCharacters = spyOn(component, 'getCharacters');
+      component.addingCharacter = true;
+
+      component.close(null);
+      expect(component.addingCharacter).toEqual(false);
+      expect(getCharacters).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('#deleteCharacter', () => {
+    const event = new Event('click');
+    const deleteCharacter = new Character(1);
+
+    beforeEach(() => {
+      component.characters = [deleteCharacter, new Character(2), new Character(3)];
+      component.selectedCharacter = deleteCharacter;
+    });
+
+    it('should stop propagating the click event', () => {
+      const stopPropagation = spyOn(event, 'stopPropagation');
+
+      component.deleteCharacter(deleteCharacter, event);
+      expect(stopPropagation).toHaveBeenCalled();
+    });
+
+    it('should delete the character', () => {
+      component.deleteCharacter(deleteCharacter, event);
+      expect(characterService.delete).toHaveBeenCalledWith(deleteCharacter);
+    });
+
+    it('should remove the deleted character from set of characters', fakeAsync(() => {
+      characterService.delete = jasmine.createSpy('delete')
+        .and.returnValue(new Promise((resolve, reject) => resolve(deleteCharacter)));
+      component.deleteCharacter(deleteCharacter, event);
+      tick();
+      expect(component.characters.find(character => character === deleteCharacter)).toBeUndefined();
+      expect(component.selectedCharacter).toBeNull()
+    }));
+
+    it('should display error message if on error', fakeAsync(() => {
+      const error = 'Noooooo!!!';
+      characterService.delete = jasmine.createSpy('delete')
+        .and.returnValue(new Promise((resolve, reject) => reject(error)));
+
+      component.deleteCharacter(deleteCharacter, event);
+      tick();
+      expect(component.error).toEqual(error);
+    }));
   });
 
   describe('#onSelect', () => {
@@ -47,6 +133,12 @@ describe('CharactersComponent', () => {
       expect(component.selectedCharacter).toBe(character);
     });
 
-    it('should set addingCharacter to false');
+    it('should set addingCharacter to false', () => {
+      const character = new Character();
+      component.addingCharacter = true;
+
+      component.onSelect(character);
+      expect(component.addingCharacter).toEqual(false);
+    });
   });
 });
